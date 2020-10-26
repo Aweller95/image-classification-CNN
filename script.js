12af46640c0c7b0dd28e597c24d7f6b67cefc44b
Alex Weller <aliweller68@hotmail.com>
2020-10-26 12:14:41 +0000

    Working image classification + result display

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const modelURL = 'https://teachablemachine.withgoogle.com/models/xZaleX1J0/';
+const modelURL = 'https://teachablemachine.withgoogle.com/models/5Ym0s1f5Y/';
 
 const modelLoaded = () =>  {
   console.log('Model Loaded!');
@@ -19,12 +19,16 @@ function createImageThumbnail(files) {
 		const preview = document.getElementById('img_container');
 		
 		if (!file.type.startsWith('image/')){ continue }
-		
+        
+    const div = document.createElement('div');
+    div.id = `${i}`;
+    preview.appendChild(div);
+
 		const img = document.createElement("img");
-		img.classList.add("image");
+    img.classList.add("image");
     img.file = file;
     img.crossorigin = 'anonymous';
-		preview.appendChild(img); // Assuming that "preview" is the div output where the content will be displayed.
+		div.appendChild(img); 
 		
 		const reader = new FileReader();
 		reader.onload = (function(aImg) { return function(e) { aImg.src = e.target.result; }; })(img);
@@ -32,11 +36,30 @@ function createImageThumbnail(files) {
   }
 }
 
+const labelImages = (data) => {
+  for(let i = 0; i <= data.length; i++){
+    const labelData = {};
+    
+    data[i].forEach(resultObj => {
+      labelData[resultObj.label] = `${(resultObj.confidence * 100).toFixed(2)}%`;
+    })
+    
+    const div = document.getElementById(`${i}`);
+    const label = document.createElement('p');
+    label.innerHTML = JSON.stringify(labelData, null, 4);
+    
+    div.appendChild(label);
+  }
+}
+
 const startClassification = () => {
   const imageFiles = document.getElementById('input').files;
   createImageThumbnail(imageFiles);
-  
+
   setTimeout(() => {
-    classify().then(result => console.log(result));
+    classify().then(result => {
+      console.log('result', result)
+      labelImages(result);// pass result to agent => pass agent result to labelImages()
+    });
   }, 500);
 }
