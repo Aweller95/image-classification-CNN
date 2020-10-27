abfa086d2abb345c4088877b6641142c514fb072
Alex Weller <aliweller68@hotmail.com>
2020-10-27 11:48:56 +0000

    working with multiple classifiers

--- script.js
+++ script.js
@@ -1,65 +1,110 @@
-const modelURL = 'https://teachablemachine.withgoogle.com/models/5Ym0s1f5Y/';
+const genderModel = 'https://teachablemachine.withgoogle.com/models/N4LSeLyfv/';
+const withHatModel =
+  'https://teachablemachine.withgoogle.com/models/F-uJf71tq/';
+const withGlassesModel = '';
 
-const modelLoaded = () =>  {
-  console.log('Model Loaded!');
-  document.getElementById("go").addEventListener("click", startClassification);
-}
+const modelLoaded = () => {
+  console.log('gender model loaded!');
+  document.getElementById('go').addEventListener('click', startClassification);
+};
 
-const classifier = ml5.imageClassifier(`${modelURL}model.json`, modelLoaded);
+const genderClassifier = ml5.imageClassifier(
+  `${genderModel}model.json`,
+  modelLoaded,
+);
+const hasHatClassifier = ml5.imageClassifier(`${withHatModel}model.json`, () =>
+  console.log('hasHatClassifier model loaded!'),
+);
+const hasGlassesClassifier = ml5.imageClassifier(
+  `${withGlassesModel}model.json`,
+  () => console.log('hasGlassesClassifier model loaded!'),
+);
 
-const classify = () => {
+const classify = classifier => {
   const images = document.getElementsByClassName('image');
 
-  return Promise.all([...images].map(img => classifier.classify(img, (err, results) => results)));
-}
+  return Promise.all(
+    [...images].map(img => classifier.classify(img, (err, results) => results)),
+  );
+};
 
 function createImageThumbnail(files) {
-	for (let i = 0; i < files.length; i++) {
-		const file = files[i];
-		const preview = document.getElementById('img_container');
-		
-		if (!file.type.startsWith('image/')){ continue }
-        
+  for (let i = 0; i < files.length; i++) {
+    const file = files[i];
+    const preview = document.getElementById('img_container');
+
+    if (!file.type.startsWith('image/')) {
+      continue;
+    }
+
     const div = document.createElement('div');
     div.id = `${i}`;
+    div.classList.add('image_div');
     preview.appendChild(div);
 
-		const img = document.createElement("img");
-    img.classList.add("image");
+    const img = document.createElement('img');
+    img.classList.add('image');
     img.file = file;
     img.crossorigin = 'anonymous';
-		div.appendChild(img); 
-		
-		const reader = new FileReader();
-		reader.onload = (function(aImg) { return function(e) { aImg.src = e.target.result; }; })(img);
-		reader.readAsDataURL(file);
+    div.appendChild(img);
+
+    const reader = new FileReader();
+    reader.onload = (function(aImg) {
+      return function(e) {
+        aImg.src = e.target.result;
+      };
+    })(img);
+    reader.readAsDataURL(file);
   }
 }
 
-const labelImages = (data) => {
-  for(let i = 0; i <= data.length; i++){
-    const labelData = {};
-    
-    data[i].forEach(resultObj => {
-      labelData[resultObj.label] = `${(resultObj.confidence * 100).toFixed(2)}%`;
-    })
-    
-    const div = document.getElementById(`${i}`);
-    const label = document.createElement('p');
-    label.innerHTML = JSON.stringify(labelData, null, 4);
-    
-    div.appendChild(label);
+const processResults = data => {
+  const labelData = [];
+
+  for (let i = 0; i < data.length; i++) {
+    let labelObj = {};
+
+    labelObj[data[i][0].label] = `${(data[i][0].confidence * 100).toFixed(2)}%`;
+    labelObj[data[i][1].label] = `${(data[i][1].confidence * 100).toFixed(2)}%`;
+    labelData.push(labelObj);
   }
-}
+
+  return labelData;
+};
+
+const labelImages = data => {
+  for (let i = 0; i < data.length; i++) {
+    const div = document.getElementById(`${i}`);
+    let labelElement = document.getElementById(`label${i}`);
+
+    if (labelElement) {
+      labelElement.innerHTML = `${labelElement.innerHTML}${JSON.stringify(
+        data[i],
+        null,
+        4,
+      )}`;
+    } else {
+      labelElement = document.createElement('p');
+      labelElement.id = `label${i}`;
+      labelElement.innerHTML = JSON.stringify(data[i], null, 4);
+      div.appendChild(labelElement);
+    }
+  }
+};
 
 const startClassification = () => {
   const imageFiles = document.getElementById('input').files;
   createImageThumbnail(imageFiles);
 
   setTimeout(() => {
-    classify().then(result => {
-      console.log('result', result)
-      labelImages(result);// pass result to agent => pass agent result to labelImages()
+    classify(genderClassifier).then(result => {
+      labelImages(processResults(result));
     });
+    classify(hasHatClassifier).then(result => {
+      labelImages(processResults(result));
+    });
+    // classify(hasGlassesClassifier).then(result => {
+    //   labelImages(processResults(result));
+    // });
   }, 500);
-}
+};
